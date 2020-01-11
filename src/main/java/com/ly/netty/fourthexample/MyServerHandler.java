b6d1bf619df8748251ccdd875bc5759fe507f4d1
ly <445671082@qq.com>
2020-01-11 21:49:12 +0800

    heartbeat detection

---
+++ src/main/java/com/ly/netty/fourthexample/MyServerHandler.java
@@ -0,0 +1,36 @@
+package com.ly.netty.fourthexample;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.timeout.IdleStateEvent;
+
+/**
+ * @author limbo Created on 2020/1/11.
+ */
+public class MyServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent){
+            IdleStateEvent event = (IdleStateEvent) evt;
+
+            String eventType = null;
+
+            switch (event.state()) {
+                case READER_IDLE:
+                    eventType = "读空闲";
+                    break;
+                case WRITER_IDLE:
+                    eventType = "写空闲";
+                    break;
+                case ALL_IDLE:
+                    eventType = "读写空闲";
+                    break;
+                default:
+            }
+
+            System.out.println(ctx.channel().remoteAddress() + " : " + eventType);
+            ctx.channel().close();
+        }
+    }
+}
